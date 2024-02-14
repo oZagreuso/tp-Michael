6997a38fed14d1d958bdd2a60e1b14e54c7ef02b
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2024-02-14 14:03:09 +0100

    140224PM1403

---
+++ cities/src/Entity/Pays.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\PaysRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaysRepository::class)]
+#[ApiResource]
+class Pays
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 2)]
+    private ?string $codePays = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomPays = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCodePays(): ?string
+    {
+        return $this->codePays;
+    }
+
+    public function setCodePays(string $codePays): static
+    {
+        $this->codePays = $codePays;
+
+        return $this;
+    }
+
+    public function getNomPays(): ?string
+    {
+        return $this->nomPays;
+    }
+
+    public function setNomPays(string $nomPays): static
+    {
+        $this->nomPays = $nomPays;
+
+        return $this;
+    }
+}
